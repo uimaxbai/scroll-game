81aaa3ca118f597aeda3301b720c8c078999322f
uimaxbai <61615730+uimaxbai@users.noreply.github.com>
2025-04-08 15:06:50 +0000

    Ill work on it later

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<style lang="scss">
+
+</style>
+
+
+
+<script lang="ts">
+
+</script>
